813defb9454a899f8bb3553794d666816ef37c6d
jSalanesh <joseph.sala2001@gmail.com>
2024-01-10 20:01:40 +0800

    Finished with customer facing side of the website

--- customer-app/index.php
+++ customer-app/index.php
@@ -7,131 +7,78 @@
     <title>Shaman Shawarma</title>
     <link rel="shortcut icon" href="img/shaman-logo.png" type="image/x-icon">
     <!-- css includes -->
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
             include "navbar.html";
         ?>
-    <div class="container-fluid shaman-background text-center">
+    <div class="container-fluid text-center text-bg-dark pb-3">
         <about class="row pt-5">
-            <h1 class="">About Us</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cumque quas consequuntur deserunt
-                error corporis labore accusantium praesentium distinctio quia! Error rem facere natus minus, vero ab
-                laborum optio in!
-                Soluta, iusto laudantium magnam quod fugit expedita eligendi, neque doloremque distinctio, iure dolores!
-                Ex, fugit aut. Ipsum ad adipisci optio quos, illum voluptatem, labore dolor alias architecto rem libero?
-                Ipsum?
-                Ex, veritatis libero voluptatem eaque quos omnis laborum expedita magnam accusantium nemo perspiciatis!
-                Aspernatur cum ullam, ducimus tempore corrupti sint quibusdam perspiciatis voluptatem quas numquam
-                dolorum non delectus tempora laborum.
-                Distinctio officiis vero fugit delectus animi rerum sunt, mollitia tenetur corrupti dicta cumque minus
-                tempora ab sit quos tempore neque maxime eveniet dolores quas. Temporibus fugiat tenetur sequi quidem
-                provident.
-                Voluptate velit accusamus tenetur blanditiis nihil aspernatur esse sunt minima a. Vitae exercitationem
-                perferendis vero inventore corporis, sint iusto. Nobis blanditiis vel nemo, illum exercitationem dolore
-                quisquam maxime doloremque provident.
-                Quae iusto, debitis at ducimus dicta perspiciatis excepturi temporibus repudiandae. Possimus iure minus
-                tenetur explicabo aspernatur ipsa quasi dolore eum dolorum quisquam ipsam, laudantium saepe dolorem nisi
-                fugiat quas. Error.
-                Aspernatur vel eius quasi rerum magnam inventore nihil ab sapiente, nam consequuntur. Harum, quasi.
-                Fugiat a tempora commodi nulla quos? Necessitatibus ipsa suscipit reprehenderit maxime quibusdam dolore
-                aspernatur voluptas ducimus?
-                Aspernatur sequi neque deleniti magni ut praesentium ipsum facilis assumenda nostrum ipsam? Numquam,
-                blanditiis corporis similique pariatur omnis quae delectus ea laborum suscipit accusamus dolor rerum,
-                commodi provident aspernatur eius!
-                Necessitatibus libero vero quas autem, soluta officia tempore ullam porro iure debitis omnis at animi
-                nesciunt alias ipsum provident, fuga voluptatum minus? Magni recusandae commodi quis cupiditate, nihil
-                rerum quod?
-                Labore expedita iure nemo perspiciatis libero necessitatibus ratione! Numquam suscipit expedita nihil
-                possimus neque dolores vel molestias, adipisci ducimus officiis quis sapiente explicabo aliquam
-                laudantium illum sed aliquid, tempore quae!
-                Inventore enim numquam ipsam eum, repellendus voluptate labore, culpa necessitatibus odio impedit
-                adipisci explicabo? Nesciunt ipsa, velit adipisci aliquam ratione deleniti perferendis vero sequi
-                aliquid, excepturi modi asperiores rem debitis.
-                Accusantium optio ipsam, error aliquam necessitatibus nostrum labore libero, odio recusandae impedit rem
-                quas suscipit quaerat atque possimus? Labore unde accusantium voluptatum eligendi nihil ratione adipisci
-                enim distinctio perspiciatis porro.
-                Hic quam reprehenderit minima voluptas cumque dolor, dignissimos facere sapiente eaque, quis repudiandae
-                numquam quia inventore, ea pariatur officiis at ipsam ullam nesciunt. Perferendis quaerat dolorem
-                sapiente expedita, ratione itaque!
-                Quibusdam accusantium officia facilis tempora natus molestiae soluta laboriosam eaque quos delectus,
-                aliquam omnis exercitationem dolor possimus molestias nemo nihil itaque quasi. Quasi architecto nulla,
-                ipsum repellat eaque laudantium ad?
-                Laboriosam explicabo, deserunt dolorum expedita labore repellendus debitis consectetur dicta rem. Saepe
-                sint magni et eum voluptatibus aperiam totam quibusdam reprehenderit, sit asperiores labore beatae
-                incidunt aut accusamus, voluptas omnis.
-                Nobis dolorem corrupti sunt fugiat facilis magni tempora est iure maxime placeat delectus mollitia,
-                quisquam repudiandae quod amet! Consequatur ad quod animi officia aliquam officiis explicabo voluptates
-                recusandae quaerat quasi?
-                Perspiciatis obcaecati laboriosam velit sequi non! Quidem praesentium neque magni perferendis culpa,
-                enim alias. Modi, harum est error hic expedita architecto placeat numquam reprehenderit dicta, rem amet?
-                Esse, animi minima.
-                Suscipit quo doloremque culpa. Officiis ad, commodi itaque ducimus ut dignissimos voluptatem natus quas
-                accusamus optio provident, quidem consequuntur explicabo quis voluptatibus libero velit. Dignissimos
-                accusantium repudiandae aut deserunt optio!
-                Quo rem, ad nam repellendus neque ipsa cum! Nemo, excepturi tempore maxime odio, ex accusantium
-                reprehenderit quae architecto mollitia in vitae facilis at ut dolorem neque sunt dicta nihil numquam!
-                Saepe sapiente nisi, voluptatum magni molestias reiciendis molestiae laboriosam numquam animi doloremque
-                natus earum maxime accusantium voluptates excepturi sit? Adipisci iure itaque unde voluptate nemo
-                molestiae corrupti reprehenderit sint debitis.
-            </p>
+            <h1>About Us</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>What we offer</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod voluptate molestias laudantium
+                        totam quo, explicabo sunt blanditiis delectus itaque necessitatibus repellendus esse rem quos
+                        qui? Impedit eaque reiciendis officia! Numquam.
+                        Ipsam impedit consectetur voluptatibus a est quo obcaecati excepturi, dolore illo porro
+                        voluptatem veniam, exercitationem repudiandae corrupti velit officiis. Saepe non veniam libero!
+                        Facere enim aspernatur dolorem quidem harum eligendi.
+                        Aliquid itaque dicta, autem minima iste dolor sapiente ipsam dolorum eius ducimus ut? Amet fuga
+                        nostrum iure accusantium non maiores natus quidem provident eius vitae? Minima provident ab quod
+                        voluptate?
+                        Beatae doloremque facilis officia commodi neque exercitationem modi! Ipsum, vel nisi accusamus
+                        atque eum id. Minima saepe deserunt odit voluptate atque optio earum aperiam commodi quibusdam.
+                        Nam veniam dolorem facilis!
+                        Ex a possimus fuga architecto alias ad? Sapiente, numquam. Porro harum voluptate et earum
+                        quidem, ipsam repellendus, deserunt maiores consequuntur fuga omnis recusandae. Reiciendis iure
+                        fugit cupiditate perferendis ipsa maxime.</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="img/shaman-background.jpg" alt="food offerings" style="width:30rem;">
+                </div>
+            </div>
         </about>
-        <menu class="row">
-            <div class="col-md-4 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+        <div class="row">
+          <h1>Where We Are</h1>
+        </div>
+        <div id="branchCarousel" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active c-item">
+                    <img src="img/bacayan-branch.jpg" class="d-block w-100 c-img" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2>Bacayan Branch</h2>
+                        <p>Our first location.</p>
+                        <a href="http://" class="text-light">Google Maps</a>
+                    </div>
+                </div>
+                <div class="carousel-item c-item">
+                    <img src="img/talamban-branch.jpg" class="d-block w-100 c-img" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2>Talamban Branch</h2>
+                        <p>Our second location.</p>
+                        <a href="http://" class=" text-light">Google Maps</a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </menu>
-        <contact class="row contact-color text-light pt-3">
-            <h1>Contact Us</h1>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, qui! Magni commodi ipsum ab
-                quisquam magnam qui ex, nam omnis adipisci perferendis tempore explicabo consequuntur, aperiam corrupti
-                id rem. Odit.
-                Quo dolorum vero aperiam impedit sit molestias, sint assumenda amet, cupiditate veniam quos eius
-                doloribus repellat perferendis, iure suscipit deserunt repellendus saepe. Facere consequuntur tempore
-                reprehenderit perferendis magnam nulla error?
-                Animi nesciunt eos nam repellat asperiores autem expedita at ea dolorem et, molestiae, non nisi adipisci
-                voluptate iste tenetur. Dolore dicta aut minus officiis excepturi harum quibusdam sunt sapiente quaerat.
-                Vitae quas doloremque ea dolores maiores sed quaerat? Porro suscipit amet facilis temporibus impedit,
-                rem est molestiae dignissimos numquam vero blanditiis veniam omnis tenetur quod fuga quae eius! Dolorum,
-                consequuntur!
-                Aliquid quisquam exercitationem illo perferendis tempore beatae soluta natus tempora ea harum aliquam
-                ratione, ipsum consequuntur in perspiciatis ullam tenetur non corporis, dignissimos incidunt quam!
-                Consequuntur distinctio ducimus quos debitis.
-            </p>
-        </contact>
-
+            <button class="carousel-control-prev" type="button" data-bs-target="#branchCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#branchCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </div>
+    <?php
+        include "contact.html";
+        ?>
     <!-- javascript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
---
+++ customer-app/menu.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shaman Menu</title>
+    <link rel="shortcut icon" href="img/shaman-logo.png" type="image/x-icon">
+    <!-- css includes -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    include "navbar.html";
+    ?>
+    <div class="container-fluid text-center text-bg-dark pb-3">
+        <div class="row">
+            <h1>Our Menu</h1>
+        </div>
+        <div class="row">
+            <h2>Specials</h2>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-overload.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Overload Box</h5>
+                        <p class="card-text">Two Tacos, 4 quesadillas, and some nachos all in one box. A saver to be sure.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 299</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        <div class="row">
+            <h2>Wraps</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-shawarma-wrap.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Shawarma Wrap</h5>
+                        <p class="card-text">Spiced beef with cucumber, tomatoes, and onions on a pita.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 89</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-burrito.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Chicken Tikka Wrap</h5>
+                        <p class="card-text">Grillled chicken in a tomato yoghurt sauce topped with parsley.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 99</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-burrito.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Shawarma Burrito</h5>
+                        <p class="card-text">Spiced beef with turmeric rice, cucumber, tomatoes and onions.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 99</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h2>Rice Bowls</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-shawarma-rice.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Shawarma Rice</h5>
+                        <p class="card-text">Spiced beef with turmeric rice, cucumber, tomatoes, and onions with sunny side egg.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 99</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-chicken-tikka-bowl.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Chicken Tikka Masala</h5>
+                        <p class="card-text">Grillled chicken in a tomato yoghurt sauce topped with parsley. Accompanied with turmic rice and served in a bowl.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 99</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h2>Mexican Snacks</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-nachos.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Nachos</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                            <h3 class="card-text text-success-emphasis">&#8369; 115</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-taco.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Tacos</h5>
+                        <p class="card-text">Hardshell tacos with spiced beef, cabbage, tomato relish and topped with shredded cheddar.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 110</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">    
+                    <img src="img/shaman-quesa.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Quesadilla</h5>
+                        <p class="card-text">Toasted pita with mozarella, spiced beef, tomatoes, and onions.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 100</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h2>Asian</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-ramen.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Cheese Ramen</h5>
+                        <p class="card-text">Mild spicy ramen topped with cheddar, chasu, egg, and seaweed.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 129</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/shaman-bibimbap.jpg" class="card-img-top c-menu" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Bibimbap</h5>
+                        <p class="card-text">Thinly sliced carrots, cabbage, cucumber, and onions with turmeric rice and gochugang sauce, topped with egg and seaweed.</p>
+                        <h3 class="card-text text-success-emphasis">&#8369; 129</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+            <h2>Add-ons</h2>
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Minced Beef
+                    <span class="badge bg-warning">25 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Nacho Chips
+                    <span class="badge bg-warning">25 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Turmeric Rice
+                    <span class="badge bg-warning">20 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Mozarella
+                    <span class="badge bg-warning">20 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Cheddar Slice
+                    <span class="badge bg-warning">10 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Chausu Pork Slice
+                    <span class="badge bg-warning">20 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Cheese Sauce
+                    <span class="badge bg-warning">15 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Shawarma Sauce
+                    <span class="badge bg-warning">20 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Kimchi
+                    <span class="badge bg-warning">25 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Egg
+                    <span class="badge bg-warning">15 PHP</span>
+                </li>
+            </ul>
+            </div>
+            <div class="col-md-1"></div>
+            
+        </div>
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+            <h2>Drinks</h2>
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Bottled Water
+                    <span class="badge bg-warning">15 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Coke, Sprite, Royal
+                    <span class="badge bg-warning">15 PHP</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Mango Tapioca/Pandan
+                    <span class="badge bg-warning">60 PHP</span>
+                </li>
+            </ul>
+            </div>
+            <div class="col-md-1"></div>
+            
+        </div>
+    </div>
+    <?php
+    include "contact.html";
+    ?>
+</body>
+
+</html>
